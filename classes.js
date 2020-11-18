b7aaf24f07aee11eccf7b25e12a331be1ad1b261
Lukas Jury <Lukas.jury13@gmail.com>
2020-11-19 12:00:28 +1300

    add functions and css

--- classes.js
+++ classes.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -20,6 +22,25 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
+function two () {
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+}
+
 // CREATE FUNCTION three HERE
 
+function three () {
+  var three = document.getElementById('three')
+
+  three.classList.add('pink')
+}
+
 // CREATE FUNCTION makeVisible HERE
+
+function makeVisible (){
+
+  var four = document.getElementsByClassName('invisible')
+  console.log(four)
+  four[0].classList.add('visible')
+}
